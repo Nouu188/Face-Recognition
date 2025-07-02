48a703aad3ea8f912970759cda375cad12a2315b
Nouu188 <dg.pthinh@gmail.com>
2025-07-02 11:00:16 +0700

    Add Procfile

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 app.register_blueprint(face_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
 from face_utils import delete_invalid_vectors
 delete_invalid_vectors()
